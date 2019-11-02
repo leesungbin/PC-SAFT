2bdefeddd88154fdd78b2df54432435277b87137
이성빈 <lsb918@uos.ac.kr>
2019-11-03 00:33:43 +0900

    add threejs & react three fiber

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './App.css';
+import { Canvas, useFrame } from 'react-three-fiber';
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Canvas>
+        <Thing />
+      </Canvas>
     </div>
   );
 }
 
+
+function Thing() {
+  const ref = useRef<any>()
+  useFrame(() => (ref.current.rotation.x = ref.current.rotation.y += 0.01))
+  return (
+    <mesh
+      ref={ref}
+      onClick={e => console.log('click')}
+      onPointerOver={e => console.log('hover')}
+      onPointerOut={e => console.log('unhover')}>
+      <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
+      <meshNormalMaterial attach="material" />
+    </mesh>
+  )
+}
 export default App;
