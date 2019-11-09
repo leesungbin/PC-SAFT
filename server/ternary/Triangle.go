4ce7a73694eefcb206aad8419e60f25e879cf6e4
이성빈 <lsb918@uos.ac.kr>
2019-11-09 15:50:00 +0900

    50 vertices to 20

--- server/ternary/Triangle.go
+++ server/ternary/Triangle.go
@@ -18,7 +18,7 @@ func Cover() (data []Plot) {
 	sq3 := math.Sqrt(3.)
 	now := Point{0.5, sq3 / 2.}
 	// 50 -> BublP : 3 sec, BublT : 12 sec ..
-	n := 50.
+	n := 20.
 	data = make([]Plot, int(sq3/4*n*n+1))
 	dx := 1. / n
 	for i := 0; ; i++ {
