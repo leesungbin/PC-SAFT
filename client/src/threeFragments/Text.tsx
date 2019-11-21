6b6c7aeee8d5ad1c74c149919f84c124fe79aea1
leesungbin <lsbskate@naver.com>
2019-11-21 18:35:10 +0900

    Delete Text.tsx

--- client/src/threeFragments/Text.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Vector3 } from 'three';
-
-// export function Text({ content, point }: { points: Vector3[]}) {
-//   return (
-//     <textGeometry />
-//   )
-// }
