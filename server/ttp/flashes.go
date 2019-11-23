598c248c548c75a5893227dfc727bdb5e381bc51
이성빈 <lsb918@uos.ac.kr>
2019-11-24 01:05:30 +0900

    flashes 작성 중

---
+++ server/ttp/flashes.go
@@ -0,0 +1,95 @@
+package ttp
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	. "github.com/leesungbin/PC-SAFT/server/api"
+	"github.com/leesungbin/PC-SAFT/server/ternary"
+)
+
+type chanFlashErr struct {
+	data FlashResult
+	err  bool
+}
+type chanFlashInput struct {
+	Z_ float64
+}
+
+func Flashes_ttp(db *sql.DB, w http.ResponseWriter, r *http.Request) {
+	var j jsonInput
+	err := json.NewDecoder(r.Body).Decode(&j)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	fmt.Printf("%v %v %v\n", j.T, j.P, j.Id)
+	res_parse, err_parse := getInfoFromBody(j)
+
+	if err_parse != nil {
+		res_json := map[string]interface{}{"status": 400, "error": err_parse}
+		print, _ := json.Marshal(res_json)
+		w.Header().Add("Content-Type", "application/json")
+		fmt.Fprintf(w, "%s", print)
+		return
+	}
+
+	rows, err := db.Query(res_parse.query)
+	if err != nil {
+		fmt.Printf("%v\n", err)
+	}
+
+	var comps Comps
+	comps.Data = make([]Component, res_parse.Nc)
+	defer rows.Close()
+	for i := 0; rows.Next(); i++ {
+		var (
+			id        int
+			component Component
+		)
+		if err := rows.Scan(
+			&id, &component.Name, &component.Mw, &component.Tc, &component.Pc, &component.Omega,
+			&component.Tb, &component.M, &component.Sig, &component.Eps, &component.K,
+			&component.E, &component.D, &component.X); err != nil {
+			fmt.Printf("err : %v\n", err)
+		}
+		comps.Data[i] = component
+	}
+
+	plots := ternary.Cover()
+	nc := len(plots)
+	inChan := make(chan chanFlashInput, nc)
+	equilDatas := make(chan chanFlashErr, nc)
+	var jsonDatas []FlashResult
+
+	for i := 0; i < nc; i++ {
+		go func(idx int) {
+			in := <-inChan
+			res, err := Flash(comps, P: j.T, T: j.T, X_: in.Z_})
+			if err != nil {
+				equilDatas <- chanFlashErr{data: FlashResult{}, err: true}
+			} else {
+				equilDatas <- chanFlashErr{data: res, err: false}
+			}
+		}(i)
+	}
+	for i := 0; i < nc; i++ {
+		a, b, c, _ := ternary.Xy2abc(plots[i].X, plots[i].Y)
+		fractions := []float64{a, b, c}
+		inChan <- chanFlashInput{Z_: fractions}
+	}
+	close(inChan)
+	
+	for i := 0; i < nc; i++ {
+		select {
+		case normal := <-equilDatas:
+			if !normal.err && normal.data.P != 0 && normal.data.T != 0 {
+				jsonDatas = append(jsonDatas, normal.data)
+			}
+		case <-time.After(5 * time.Millisecond):
+			// jsonDatas = append(jsonDatas, Eq_Result{})
+		}
+	}
+}
