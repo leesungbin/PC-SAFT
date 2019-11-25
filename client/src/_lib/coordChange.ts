9828a975261d523f485c2d0c09f03d69d51c2aaa
이성빈 <lsb918@uos.ac.kr>
2019-11-25 23:41:55 +0900

    delete react three fiber & add react konva

--- client/src/_lib/coordChange.ts
+++ client/src/_lib/coordChange.ts
@@ -1,6 +1,28 @@
-export function coordChange(a: number, b: number, c: number): ({x: number, y: number}) {
+export function coordChange(a: number, b: number, c: number): ({ x: number, y: number }) {
   const sq3 = Math.sqrt(3);
   const y = a / 2 * sq3;
   const x = c + y / sq3;
   return { x, y };
+}
+
+// points: 삼각형의 각 좌표 배열 [x1, y1, x2, y2, x3, y3]
+//   2
+// 1   3
+export function xyTransform(x: number, y: number, points: number[], ternP: number[]): ({x: number, y: number}) {
+  const comy = points[3] - points[5]; // y2-y3
+  const comx = points[2] - points[4]; // x2-x3
+  const p = ternP[0] * comy;
+  const q = ternP[1] * comy;
+  const Y = points[1] + p;
+  // y=comy/comx*(x-x2)+y2
+  // y=comy/comx*x-comy/comx*x2+y2
+  // comx*y-comy*x+comy*x2-y2*comx = 0
+  // Math.abs( comx*Y-comy*X+comy*x2-y2*comx )/Math.sqrt(comx*comx+comy*comy) == q)
+  // (1) comx*Y-comy*X+comy*x2-y2*comx = q*Math.sqrt(comx*comx+comy*comy)
+  // (2) comx*Y-comy*X+comy*x2-y2*comx  = -q*Math.sqrt(comx*comx+comy*comy)
+  const X1 = -(q * Math.sqrt(comx * comx + comy * comy) - comx * Y - comy * points[2] + points[3] * comx) / (comy);
+  const X2 = (q * Math.sqrt(comx * comx + comy * comy) - comx * Y - comy * points[2] + points[3] * comx) / (comy);
+  const line = (x: number) => { return comy / comx * (x - points[2]) + points[3] };
+  if (line(X1) < Y) return {x: X1, y: Y};
+  return {x: X2, y: Y};
 }
---
+++ client/src/canvas/Point.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Circle, Group, Text } from 'react-konva';
+
+type PointProps = {
+  setHover: (state: boolean) => void,
+  fill: string,
+  x: number,
+  y: number,
+  hover: boolean,
+  info: number[],
+};
+export default function Point({ setHover, fill, x, y, info, hover }: PointProps) {
+  return (
+    <Group>
+      <Circle onMouseOver={() => setHover(true)} onMouseOut={() => setHover(false)} radius={3} fill={fill} x={x} y={y} />
+      {hover &&
+        <Text text={
+          JSON.stringify(info, (key, val) => { return val.toFixed ? Number(val.toFixed(3)) : val })}
+          x={x} y={y} width={120}  align="center" fill="black" draggable />}
+    </Group>
+  )
+}
