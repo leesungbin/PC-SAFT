746837cd696c4509b14ff3c88775cde80ffd35ae
이성빈 <lsb918@uos.ac.kr>
2019-11-30 06:52:40 +0900

    git reset cached

--- client/src/_lib/endpoint.ts
+++
@@ -1,4 +0,0 @@
-export const EQUIL_ENDPOINT = "/api/equil";
-export const DATA_ENDPOINT = "/api/datas";
-export const SEARCH_ENDPOINT = "/api/search";
-export const FLASHES_ENDPOINT = "/api/flashes";
